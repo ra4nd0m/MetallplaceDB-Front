14eb295f0e5e94b4a0b66eabbeb032e77b150c41
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-16 18:56:50 +0300

    Material properties fetch

--- src/pages/lib/getData.ts
+++ src/pages/lib/getData.ts
@@ -1,5 +1,5 @@
 export async function getMaterials(token: string) {
-    let ret_value: materials|string;
+    let ret_value: materials | string;
     await fetch('http://localhost:3030/getMaterialList', {
         method: 'POST',
         headers: { Authorization: token, "Content-Type": "application/json" },
@@ -7,7 +7,7 @@ export async function getMaterials(token: string) {
     })
         .then((res) => {
             if (res.status != 200) {
-                throw new Error('Error retriving the data!');
+                throw new Error(`Error retriving the data!\nStatus:${res.status}`);
             }
             return res.json();
         })
@@ -21,6 +21,29 @@ export async function getMaterials(token: string) {
     return ret_value;
 }
 
+export async function getMatProps(token: string, material_id: number) {
+    let ret_value: matProps | string;
+    let payload = { material_source_id: material_id }
+    await fetch('http://localhost:3030/getPropertyList', {
+        method: 'POST',
+        headers: { Authorization: token, "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+    })
+        .then((res) => {
+            if (res.status != 200) {
+                throw new Error(`Fetch failed!\nStatus: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data)=>{
+            ret_value=data;
+        })
+        .catch((err)=>{
+            ret_value='';
+            alert(err);
+        });
+    return ret_value;
+}
 export interface materials {
     Id: number;
     Name: string;
@@ -29,4 +52,10 @@ export interface materials {
     Market: string;
     DeliveryType: string;
     Unit: string;
+}
+
+export interface matProps {
+    Id: number;
+    Name: string;
+    Kind: string;
 }
