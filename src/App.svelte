b110e532f9d48f64ce4240860fe7454c486181b4
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-16 20:02:44 +0300

    Navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   import AddMaterial from "./pages/AddMaterial.svelte";
   import Home from "./pages/Home.svelte";
   import Logout from "./pages/Logout.svelte";
-  import MaterialList from "./pages/components/MaterialList.svelte";
+  import Navbar from "./pages/components/Navbar.svelte";
   let secret;
   token.subscribe((val) => {
     localStorage.setItem("token", val);
@@ -13,26 +13,8 @@
   });
 </script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
-  <ul class="navbar-nav me-auto">
-    <li class="nav-item">
-      <a class="nav-link" href="#/">Материалы</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/#/addMaterial">Добавить материал</a>
-    </li>
-  </ul>
-  <ul class="navbar-nav ms-auto">
-    <li class="nav-item">
-      {#if secret == ""}
-        <a class="nav-link" href="/#/login">Login</a>
-      {:else}
-        <a class="nav-link" href="/#/logout">Log Out</a>
-      {/if}
-    </li>
-  </ul>
-</nav>
 
+<Navbar key={secret}/>
 <main class="p-3">
   <Router
     routes={{
