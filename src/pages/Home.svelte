687bdd31ac5325c55797b5f12f8e97b590a71e3a
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-16 20:03:26 +0300

    Proper home

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,3 +1,14 @@
-<script>
-
+<script lang="ts">
+    import { token } from "./lib/stores";
+    import MaterialList from "./components/MaterialList.svelte";
+    import { push } from "svelte-spa-router";
+    let secret: string;
+    token.subscribe((value) => (secret = value));
 </script>
+
+<div>
+{#if secret.length != 0}
+    <MaterialList  />
+{:else}{push("/login")}
+{/if}
+</div>
