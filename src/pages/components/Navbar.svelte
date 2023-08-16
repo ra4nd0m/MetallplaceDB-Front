d1e2855adc25b20b7b3b2fdce72a457e76f7517a
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-16 20:01:49 +0300

    Navbar component

---
+++ src/pages/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let key: string = "";
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
+    <ul class="navbar-nav me-auto">
+        {#if key != ""}
+            <li class="nav-item">
+                <a class="nav-link" href="#/">Материалы</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/#/addMaterial">Добавить материал</a>
+            </li>
+        {/if}
+    </ul>
+    <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            {#if key == ""}
+                <a class="nav-link" href="/#/login">Login</a>
+            {:else}
+                <a class="nav-link" href="/#/logout">Log Out</a>
+            {/if}
+        </li>
+    </ul>
+</nav>
