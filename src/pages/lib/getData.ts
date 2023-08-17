5cf93f757e021e2c00a407b00dd2f195a6ff08fd
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-17 12:19:17 +0300

    Code cleanup

--- src/pages/lib/getData.ts
+++ src/pages/lib/getData.ts
@@ -1,45 +1,34 @@
 export async function getMaterials(token: string) {
-    let ret_value: materials | string;
-    await fetch('http://localhost:3030/getMaterialList', {
+    let ret_value: any;
+    let payload = '';
+    ret_value = await doFetch(payload, '/getMaterialList', token)
+    return ret_value;
+}
+
+export async function getMatProps(token: string, material_id: number) {
+    let ret_value: any;
+    let payload = JSON.stringify({ material_source_id: material_id });
+    ret_value = await doFetch(payload,'/getPropertyList',token);
+    return ret_value;
+}
+
+async function doFetch(payload: string, address: string, token: string) {
+    let ret_value: matProps[] | materials[] | string;
+    await fetch(`http://localhost:3030${address}`, {
         method: 'POST',
         headers: { Authorization: token, "Content-Type": "application/json" },
-        body: ''
+        body: payload,
+    }).then((res) => {
+        if (res.status != 200) {
+            throw new Error(`Fetch failed!\nStatus: ${res.status}`);
+        }
+        return res.json();
     })
-        .then((res) => {
-            if (res.status != 200) {
-                throw new Error(`Error retriving the data!\nStatus:${res.status}`);
-            }
-            return res.json();
-        })
         .then((data) => {
             ret_value = data.list;
         })
         .catch((err) => {
             ret_value = '';
-            alert(err);
-        });
-    return ret_value;
-}
-
-export async function getMatProps(token: string, material_id: number) {
-    let ret_value: matProps[] | string;
-    let payload = { material_source_id: material_id }
-    await fetch('http://localhost:3030/getPropertyList', {
-        method: 'POST',
-        headers: { Authorization: token, "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
-    })
-        .then((res) => {
-            if (res.status != 200) {
-                throw new Error(`Fetch failed!\nStatus: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data)=>{
-            ret_value=data.list;
-        })
-        .catch((err)=>{
-            ret_value='';
             alert(err);
         });
     return ret_value;
