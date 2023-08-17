3aed0d3517e039478c568589aa6c9582da8342cc
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-17 17:41:02 +0300

    Added validation

--- src/pages/components/AddMaterial.svelte
+++ src/pages/components/AddMaterial.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { materials_data } from "../lib/stores";
     import { postMaterial, type material } from "../lib/getData";
+    import * as yup from "yup";
     export let secret: string;
     let name: string;
     let source: string;
@@ -8,7 +9,23 @@
     let market: string;
     let delivery_type: string;
     let unit: string;
-
+    const validation_schema = yup.object().shape({
+        Name: yup.string().required("Требуется название!"),
+        Source: yup.string().required("Требуется источник!"),
+        Group: yup.string().required("Требуется группа!"),
+        Market: yup.string().required("Требуется рынок!"),
+        DeliveryType: yup.string().required("Требуется тип поставки!"),
+        Unit: yup.string().required("Требуется еденица!"),
+    });
+    async function validateInput(material: material) {
+        try {
+            await validation_schema.validate(material, { abortEarly: false });
+            return 0;
+        } catch (error) {
+            let validationErrors = error;
+            return validationErrors;
+        }
+    }
     const addMaterial = async () => {
         let newMaterial: material = {
             Name: name,
@@ -18,6 +35,12 @@
             DeliveryType: delivery_type,
             Unit: unit,
         };
+        console.log(newMaterial);
+        let errors = await validateInput(newMaterial);
+        if (errors != 0) {
+            alert("Ошибка!\nНе все поля заполнены!");
+            return;
+        }
         name = "";
         source = "";
         group = "";
@@ -29,18 +52,64 @@
         });
         materials_data.update((items) => [...items, newMaterial]);
     };
-
 </script>
 
 <tr>
     <td />
-    <td><input class="form-control" type="text" bind:value={name} placeholder="Наименование" required /></td>
-    <td><input class="form-control" type="text" bind:value={source} placeholder="Источник данных"/></td>
-    <td><input class="form-control" type="text" bind:value={group}  placeholder="Группа"/></td>
-    <td><input class="form-control" type="text" bind:value={market} placeholder="Рынок" /></td>
-    <td><input class="form-control" type="text" bind:value={delivery_type} placeholder="	Тип поставки"/></td>
-    <td><input class="form-control" type="text" bind:value={unit} placeholder="Еденица" /></td>
-    <td><button class="btn btn-success" on:click={async () => await addMaterial()}>Добавить</button></td
+    <td
+        ><input
+            class="form-control"
+            type="text"
+            bind:value={name}
+            placeholder="Наименование"
+            required
+        /></td
     >
-    <td></td>
+    <td
+        ><input
+            class="form-control"
+            type="text"
+            bind:value={source}
+            placeholder="Источник данных"
+        /></td
+    >
+    <td
+        ><input
+            class="form-control"
+            type="text"
+            bind:value={group}
+            placeholder="Группа"
+        /></td
+    >
+    <td
+        ><input
+            class="form-control"
+            type="text"
+            bind:value={market}
+            placeholder="Рынок"
+        /></td
+    >
+    <td
+        ><input
+            class="form-control"
+            type="text"
+            bind:value={delivery_type}
+            placeholder="	Тип поставки"
+        /></td
+    >
+    <td
+        ><input
+            class="form-control"
+            type="text"
+            bind:value={unit}
+            placeholder="Еденица"
+        /></td
+    >
+    <td
+        ><button
+            class="btn btn-success"
+            on:click={async () => await addMaterial()}>Добавить</button
+        ></td
+    >
+    <td />
 </tr>
