6b7de7e1b7b6fe1bec127d170a8aa68214afb6b1
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-17 15:44:16 +0300

    type fixes

--- src/pages/lib/stores.ts
+++ src/pages/lib/stores.ts
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 
 export const token = writable(localStorage.getItem("token") || "");
-export const materials_data = writable(JSON.parse(localStorage.getItem("materials_data"))||{});
+export const materials_data = writable(JSON.parse(localStorage.getItem("materials_data"))||[]);
