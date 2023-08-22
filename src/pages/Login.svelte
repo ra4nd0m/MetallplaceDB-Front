267a64c6892e529e48128b7b18f6e790474c7e35
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-08-22 20:22:38 +0300

    Fixed login adress

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -8,7 +8,7 @@
             username: login,
             password: pass,
         };
-        fetch("http://localhost:3030/login", {
+        fetch(`${import.meta.env.VITE_API_URL}/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload),
