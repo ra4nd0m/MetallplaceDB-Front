13952af0a227253587b837aef75a44a5bcfa72ac
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-09-12 12:25:41 +0300

    Средняя цена должна рассчитываться автоматически #6 Будет работать только при постоянных значениях свойств

--- src/pages/components/AddRecord.svelte
+++ src/pages/components/AddRecord.svelte
@@ -1,31 +1,16 @@
 <script lang="ts">
-    import { onMount } from "svelte";
     import { doFetch, type matProp } from "../lib/getData";
     import Flatpickr from "svelte-flatpickr";
     import "flatpickr/dist/flatpickr.css";
-    let material_props_list: propVal[];
     export let mat_id: number;
     export let secret: string;
     let created_on;
-    let minPrice: number;
-    let maxPrice: number;
-    let avgPrice: number;
+    let minPrice;
+    let maxPrice;
+    let avgPrice;
     //Works only when prop ids are static
     //Better to utilise backend functionality
-    const avgPriceId = 1;
-    const minPriceId = 2;
-    const maxPriceId = 3;
-    onMount(async () => {
-        let payload = JSON.stringify({ material_source_id: `${mat_id}` });
-        material_props_list = await doFetch(
-            payload,
-            "/getPropertyList",
-            secret
-        ).then((val) => {
-            return val.list;
-        });
-        console.log(material_props_list);
-    });
+
     function calculateAvgPrice() {
         if (typeof minPrice != "undefined" && typeof maxPrice != "undefined") {
             avgPrice = (minPrice + maxPrice) / 2;
@@ -34,12 +19,44 @@
     async function submitRecord() {
         //Works only when prop ids are static
         //Better to utilise backend functionality
+        let alertMessage = "";
+        if (created_on === "") alertMessage += "Дата не задана!\n";
+
+        if (typeof minPrice === "undefined" || minPrice === "")
+            alertMessage += "Минимальная цена не задана!\n";
+
+        if (typeof maxPrice === "undefined" || maxPrice === "")
+            alertMessage += "Максимальная цена не задана!\n";
+        if (alertMessage !== "") {
+            alert(alertMessage);
+            return;
+        }
         let payloadAvgPrice = {
             material_source_id: mat_id,
             property_name: "Средняя цена",
-            value_float: avgPrice,
-            value_str:avgPrice
+            value_float: `${avgPrice}`,
+            value_str: `${avgPrice}`,
+            created_on: created_on,
         };
+        let payloadMinPrice = {
+            material_source_id: mat_id,
+            property_name: "Мин цена",
+            value_float: `${minPrice}`,
+            value_str: `${minPrice}`,
+            created_on: created_on,
+        };
+        let payloadMaxPrice = {
+            material_source_id: mat_id,
+            property_name: "Макс цена",
+            value_float: `${maxPrice}`,
+            value_str: `${maxPrice}`,
+            created_on: created_on,
+        };
+        await doFetch(JSON.stringify(payloadAvgPrice), "/addValue", secret);
+        await doFetch(JSON.stringify(payloadMinPrice), "/addValue", secret);
+        await doFetch(JSON.stringify(payloadMaxPrice), "/addValue", secret);
+
+        console.log(payloadAvgPrice, payloadMinPrice, payloadMaxPrice);
         /*        let sentSomething: boolean = false;
         for (const prop of material_props_list) {
             if (typeof prop.Value != "undefined" && prop.Value !== "") {
@@ -57,56 +74,55 @@
         }
         if (!sentSomething) alert("Поля пусты!\nЗаполните хотя бы одно!");*/
     }
-    interface propVal extends matProp {
-        Value?: string;
-    }
 </script>
 
-{#if typeof material_props_list != "undefined"}
-    <div class="d-flex justify-content-center">
-        <form on:submit|preventDefault={() => {}}>
-            <div class="ms-3 mt-3">
-                <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Мин цена"
-                    bind:value={minPrice}
-                    on:input={() => calculateAvgPrice()}
-                />
-            </div>
-            <div class="ms-3 mt-3">
-                <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Макс цена"
-                    bind:value={maxPrice}
-                    on:input={() => calculateAvgPrice()}
-                />
-            </div>
-            <div class="ms-3 mt-3">
-                <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Средняя цена"
-                    bind:value={avgPrice}
-                    disabled
-                />
-            </div>
+<div class="d-flex justify-content-center">
+    <form
+        on:submit|preventDefault={async () => {
+            await submitRecord();
+        }}
+    >
+        <div class="ms-3 mt-3">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Мин цена"
+                bind:value={minPrice}
+                on:input={() => calculateAvgPrice()}
+            />
+        </div>
+        <div class="ms-3 mt-3">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Макс цена"
+                bind:value={maxPrice}
+                on:input={() => calculateAvgPrice()}
+            />
+        </div>
+        <div class="ms-3 mt-3">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Средняя цена"
+                bind:value={avgPrice}
+                disabled
+            />
+        </div>
 
-            <div class="ms-3 mt-3">
-                <Flatpickr
-                    options={{ enableTime: false }}
-                    bind:formattedValue={created_on}
-                    class="form-control"
-                    on:change={(dateStr) => {
-                        created_on = dateStr;
-                    }}
-                    placeholder="Дата"
-                />
-            </div>
-            <div class="ms-3 mt-3">
-                <button type="submit" class="btn btn-success">Добавить</button>
-            </div>
-        </form>
-    </div>
-{/if}
+        <div class="ms-3 mt-3">
+            <Flatpickr
+                options={{ enableTime: false }}
+                bind:formattedValue={created_on}
+                class="form-control"
+                on:change={(dateStr) => {
+                    created_on = dateStr;
+                }}
+                placeholder="Дата"
+            />
+        </div>
+        <div class="ms-3 mt-3">
+            <button type="submit" class="btn btn-success">Добавить</button>
+        </div>
+    </form>
+</div>
