b270e5b116cc2cab3c3714b230bcf3b87e4ff2c4
Egor Kosyakov <ra4nd0m@gmail.com>
2023-11-21 19:11:30 +0300

    Added auto load on login

--- src/pages/components/GetMaterialsList.svelte
+++ src/pages/components/GetMaterialsList.svelte
@@ -2,16 +2,23 @@
     import { push } from "svelte-spa-router";
     import { doFetch } from "../lib/getData";
     import { materials_data } from "../lib/stores";
+    import { onMount } from "svelte";
     export let secret: string;
+
+    onMount(async () => {
+        await grabData(secret);
+    });
 
     async function grabData(key: string) {
         if (key.length != 0) {
             let material_list;
-            material_list = await doFetch("{}", "/getMaterialList", secret).then(
-                (val) => {
-                    return val.list;
-                }
-            );
+            material_list = await doFetch(
+                "{}",
+                "/getMaterialList",
+                secret
+            ).then((val) => {
+                return val.list;
+            });
             materials_data.set(material_list);
             materials_data.subscribe((val) =>
                 localStorage.setItem("materials_data", JSON.stringify(val))
