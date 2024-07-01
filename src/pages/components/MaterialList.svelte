f6a7b0a4f5af0fa0177ea142d3554a0ab613fe20
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2024-07-01 22:30:22 +0300

    On table update nullref fix part 1

--- src/pages/components/MaterialList.svelte
+++ src/pages/components/MaterialList.svelte
@@ -77,9 +77,11 @@
         filteredData[id].expanded = !filteredData[id].expanded;
     }
     $: {
-        filteredData = tableData?.filter((item) =>
-            item.Name.toLowerCase().includes(search_item.toLowerCase()),
-        );
+        if(search_item !== ""){
+            filteredData = tableData?.filter((item) =>
+                item.Name.toLowerCase().includes(search_item.toLowerCase()),
+            );
+        }
         if (selectedFilters.source != "")
             filteredData = filteredData.filter((item) => {
                 return item.Source === selectedFilters.source;
