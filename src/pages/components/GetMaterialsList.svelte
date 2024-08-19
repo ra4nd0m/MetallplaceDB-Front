cbad08cd41f14f6a95681e7f041d4b150c6faf58
Egor Kosyakov <ra4nd0m@gmail.com>
2024-08-19 19:48:24 +0300

    Get last update date functionality logic

--- src/pages/components/GetMaterialsList.svelte
+++ src/pages/components/GetMaterialsList.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import { push } from "svelte-spa-router";
-    import { doFetch, type material } from "../lib/getData";
+    import { doFetch, type material, type priceFeed } from "../lib/getData";
     import { materials_data } from "../lib/stores";
     import { onMount } from "svelte";
     export let secret: string;
-
+    let bigList: material[];
     onMount(async () => {
         await grabData(secret);
     });
@@ -18,6 +18,12 @@
             } else {
                 material_list = [];
             }
+            if (material_list.length !== 0) {
+                const updateDates = await getLastUpdateDate(material_list);
+                for (let i = 0; i < material_list.length; i++) {
+                    material_list[i].update_date = updateDates[i];
+                }
+            }
             materials_data.set(material_list);
             materials_data.subscribe((val) =>
                 localStorage.setItem("materials_data", JSON.stringify(val)),
@@ -25,6 +31,34 @@
         } else {
             push("/login");
         }
+    }
+
+    async function getLastUpdateDate(mat_list: material[]) {
+        const today = new Date().toISOString().split("T")[0];
+        let updateDatesArr = [];
+        for (let matObj of mat_list) {
+            let paylad = {
+                finish: today,
+                material_source_id: matObj.Id,
+                n_values: 1,
+                property_id: 1,
+            };
+            const res = (await doFetch(
+                JSON.stringify(paylad),
+                "/getNLastValues",
+                secret,
+            ).then((val) => {
+                if (typeof val === "object" && "price_feed" in val) {
+                    return val.price_feed;
+                }
+            })) as priceFeed[];
+            if (res !== null) {
+                updateDatesArr.push(res[0].date.split("T")[0]);
+            } else {
+                updateDatesArr.push("");
+            }
+        }
+        return updateDatesArr;
     }
 </script>
 
@@ -34,3 +68,4 @@
     }}
     class="btn btn-primary">Обновить список материалов</button
 >
+
