f8635ac020e2fac35626f2863aa57cc2412f946b
Egor Kosyakov <ra4nd0m@gmail.com>
2024-10-01 12:31:25 +0300

    Improved folded menu selector

--- src/pages/components/SubmenuSwitch.svelte
+++ src/pages/components/SubmenuSwitch.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
-    export let selectedMenu = "propsList";
+    let recordsDisabled = false;
+    let listDisabled = false;
+    export let selectedMenu = "addRecord";
+    $: {
+        switch (selectedMenu) {
+            case "propsList":
+                listDisabled = true;
+                recordsDisabled = false;
+                break;
+            case "addRecord":
+                listDisabled = false;
+                recordsDisabled = true;
+                break;
+        }
+    }
 </script>
 
 <div>
-    <button class="btn btn-primary" on:click={() => (selectedMenu = "propsList")}
-        >Список свойств</button
+    <button
+        class="btn btn-primary"
+        disabled={recordsDisabled}
+        on:click={() => (selectedMenu = "addRecord")}>Добавить запись</button
     >
-    <button class="btn btn-primary" on:click={() => (selectedMenu = "addRecord")}
-        >Добавить запись</button
-    >
+    <button
+        class="btn btn-primary"
+        disabled={listDisabled}
+        on:click={() => (selectedMenu = "propsList")}>Список свойств</button
+    > 
     <slot />
 </div>
